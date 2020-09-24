a58a51093aa4088cd8518d6c83ea8e2cbb5f3870
amir <a.hallaji.b@gmail.com>
2020-09-24 15:30:01 +0330

    if else concept added

--- src/Lesson.java
+++ src/Lesson.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Lesson {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int input = scan.nextInt();
+
+        if (input > 10) {
+            System.out.println("greater than 10");
+        }
+        else if(input < 10) {
+            System.out.println("less than 10");
+        }
+        else {
+            System.out.println("10");
+        }
+
+
+    }
 }
