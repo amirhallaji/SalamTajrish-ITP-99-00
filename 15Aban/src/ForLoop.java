39f49083000a8fb878daf56748b4557a04d3f4dd
amirhallaji <a.hallaji.b@gmail.com>
2020-11-05 15:29:49 +0330

    ForLoop.java Added

--- 15Aban/src/ForLoop.java
+++ 15Aban/src/ForLoop.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class ForLoop {
     public static void main(String[] args) {
+        for (int i = 8 ; i <= 10 ; i++){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        int sum = 0;
+        for(int i = 1 ; i <= number ; i++){
+            sum += i;
+        }
+        System.out.println(sum);
 
     }
 }
