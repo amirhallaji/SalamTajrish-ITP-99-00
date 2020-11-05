cdaf50d30ecfd3f58eceb35e16f419c6263760d5
amirhallaji <a.hallaji.b@gmail.com>
2020-11-05 15:17:36 +0330

    DoWhile.java Added

---
+++ 15Aban/src/DoWhile.java
@@ -0,0 +1,15 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int i = 11;
+
+        do {
+            System.out.println(i);
+            i++;
+        }while (i <= 10);
+
+        do{
+            System.out.println(i);
+            i++;
+        }while (i <= 10);
+    }
+}
